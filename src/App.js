f3190d92f8f34694860c124640798859cb5058b1
Danish Ali <alidanish373@gmail.com>
2025-08-21 22:06:35 +0530

    Added Collection Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './index.css';
 import Home from './pages/Home';
 import NewArrivalPage from './pages/NewArrival';
 import Gifts from './pages/Gifts';
-// import Collection from './pages/Collection';
+import Collection from './pages/Collection';
 // import CustomMade from './pages/CustomMade';
 // import Contact from './pages/Contact';
 // import OurStory from './pages/OurStory';
@@ -15,7 +15,7 @@ const App = () => (
       <Route path="/" element={<Home />} />
       <Route path="/newarrival" element={<NewArrivalPage />} />
       <Route path="/gifts" element={<Gifts />} />
-      {/* <Route path="/collection" element={<Collection />} /> */}
+      <Route path="/collection" element={<Collection />} />
       {/* <Route path="/custommade" element={<CustomMade />} /> */}
       {/* <Route path="/contact" element={<Contact />} /> */}
       {/* <Route path="/ourstory" element={<OurStory />} /> */}
