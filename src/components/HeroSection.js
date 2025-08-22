825dfe1c586bd1fa31fcee1054f41034b2159e85
Danish Ali <alidanish373@gmail.com>
2025-08-22 23:29:13 +0530

    Some Changes in Hero Section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -22,8 +22,8 @@ const Hero = () => {
                 loop
             />
 
-            <div className="absolute inset-0 items-center justify-center z-10 hidden xl:flex">
-                <form onSubmit={handleSubmit} className="relative max-w-md -mt-[620px]">
+            <div className="absolute inset-0 z-10 hidden xl:flex justify-center mt-[70px]">
+                <form onSubmit={handleSubmit} className="relative max-w-md">
                     <FaSearch
                         className="absolute top-1/2 left-7 transform -translate-y-1/2 z-50 text-white cursor-pointer w-[25px] h-[29px]"
                         onClick={handleSubmit}
@@ -34,7 +34,7 @@ const Hero = () => {
                         onChange={(e) => setQuery(e.target.value)}
                         placeholder="Search"
                         className="w-[590px] h-[75px] rounded-[10px] text-center border-[3px] border-white bg-white/20 backdrop-blur-sm py-2 pl-12 pr-4 text-white text-2xl focus:outline-none focus:ring-2 focus:border-white-600 hover:border-white-400 
-                 placeholder:text-[26px] placeholder:font-semibold placeholder:leading-[120%] placeholder:tracking-[0.02em] placeholder:align-middle placeholder-white"
+        placeholder:text-[26px] placeholder:font-semibold placeholder:leading-[120%] placeholder:tracking-[0.02em] placeholder:align-middle placeholder-white"
                     />
                 </form>
             </div>
