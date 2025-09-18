8ef09097da55bb54de60b00c3589fdcd962791b8
pinnateai-2025 <pinnateai@gmail.com>
2025-09-18 19:02:07 +0530

    add pinnate technologies redirection in footer2

--- src/components/Footer2.js
+++ src/components/Footer2.js
@@ -3,7 +3,17 @@ const Footer2 = () => {
   return (
     <footer className="bg-[#0D4017] text-white text-center px-10 py-6">
       <p>&copy; {new Date().getFullYear()} The Table Gem. All rights reserved.</p> <br />
-      <p>Designed and Developed by Pinnate Technologies</p>
+       <p>
+        Designed and Developed by{" "}
+        <a 
+          href="https://pinnate.vercel.app/" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="text-white-400 hover:underline"
+        >
+          Pinnate Technologies
+        </a>
+      </p>
     </footer>
   );
 };
